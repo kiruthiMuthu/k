def065fc5b8ba27bb12811037f704be752016801
kiruthiMuthu <kiruthiIDOK@gmail.com>
2017-06-06 15:05:16 +0530

    Add files via upload

--- sk.c
+++ sk.c
@@ -1,11 +1,20 @@
 #include<stdio.h>
-int main;
+int main()
 {
 int number;
 printf("enter a number\n");
 scanf("%d,&number");
-if(number<=0)
-printf("%d is positive number\n",number);
-else
-scanf("%d is the negative number\n",number);
+if(number>=1)
+{
+printf("number is positive ");
+}
+ else if(number>=-1)
+ {
+printf("%number is  negative");
+ }
+else if(number==0)
+{
+printf("number is zero");
+}
+return 0;
 }
